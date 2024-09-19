4565f91e9c90f35fb0f5f279df15804230905b5b
Edder <enaranjo5489@uta.edu.ec>
2024-09-19 15:05:38 -0500

    Cositas

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
+import { PageLoginComponent } from './page-login/page-login.component';
+import { LoginComponent } from './login/login.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    MenuComponent,
+    PageLoginComponent,
+    LoginComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CoreModule { }
